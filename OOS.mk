3dee97c1ee0a028c7e7e66e76fa23b59038809b1
B07123 <roybot8969009@gmail.com>
2021-09-22 14:31:41 +0530

    Added some stuff along with fixes

--- OOS.mk
+++ OOS.mk
@@ -3,12 +3,18 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 PRODUCT_COPY_FILES += \
     vendor/OOS/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-launcher.xml \
     vendor/OOS/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-filemanager.xml \
-    vendor/OOS/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml
+    vendor/OOS/etc/permissions/privapp-permissions-op-screenrecord.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-screenrecord.xml \
+    vendor/OOS/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml \
+    vendor/OOS/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml
 
 PRODUCT_PACKAGES += \
     OPMms11 \
-    OPDialer\
+    OPDialer \
+    OPCamera \
     OPDeskClock \
+    OPCalculator \
+    OPNotes \
+    OPScreenRecorder \
     OPFileManager \
     OPGallery \
     OPIconpackHydrogen \
