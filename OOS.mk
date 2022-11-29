dd668ac030a0814e131336dfa3f0bad34830168f
Hyper <hyper@remainsilent.net>
2022-11-30 00:42:39 +0530

    Adopted Lawnchair from Teamfiles teams very thanks to him

--- OOS.mk
+++ OOS.mk
@@ -3,7 +3,9 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 PRODUCT_COPY_FILES += \
     vendor/OOS/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-launcher.xml \
     vendor/OOS/etc/permissions/privapp-permissions-op-screenrecord.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-screenrecord.xml \
-    vendor/OOS/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml 
+    vendor/OOS/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml \
+    vendor/OOS/etc/permissions/privapp-permissions-app.lawnchair.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-app.lawnchair.xml \
+    vendor/OOS/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml
 
 PRODUCT_PACKAGES += \
     OPDialer \
@@ -16,6 +18,9 @@ PRODUCT_PACKAGES += \
     OPIconpackSquare \
     OPLauncher \
     OPRecentsProvider \
-    OPWallpaperResources \
     OPWeather \
-    OPWidget
+    OPWidget \
+    Lawnicons \
+    Lawnfeed \
+    Lawnchair \
+    QuickSwitchOverlay
